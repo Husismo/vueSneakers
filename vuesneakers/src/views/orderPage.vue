01a865cfe534a24f10344cad13e4689d53cdfde5
Denis Zhuravlev <denis-zhuravlev-2017@mail.ru>
2023-07-30 13:23:07 +0500

    добавление валидации

--- vuesneakers/src/views/orderPage.vue
+++ vuesneakers/src/views/orderPage.vue
@@ -29,26 +29,25 @@
                                 <p class="form__title">Контакты</p>
                                 <input 
                                 v-model="order.phoneNumber"
-                                :style="{'border-color': phoneNumberError}" 
+                                :class="$v.order.phoneNumber.$error ? 'isInvalid': ''"
                                 class="form__number" placeholder="Номер телефона" type="number">
                                 <input 
                                 v-model.trim="order.email"
-                                :style="{'border-color': emailError}" 
+                                :class="$v.order.email.$error ? 'isInvalid': ''"
                                 class="form__email" placeholder="E-mail" type="email">
                                 <input 
                                 v-model.trim="order.fullName"
-                                :style="{'border-color': fullNameError}" 
+                                :class="$v.order.fullName.$error ? 'isInvalid': ''"
                                 class="form__fullName" placeholder="Фамилия Имя Отчество" type="text">
                                 <p class="form__title">адрес</p>
                                 <input 
                                 v-model.trim="order.adress"
-                                :style="{'border-color': adresError}" 
+                                :class="$v.order.adress.$error ? 'isInvalid': ''" 
                                 class="form__adress" placeholder="Город, улица, дом, квартира" type="text">
                                 <textarea v-model="order.comment" placeholder="Комментарий к заказу" class="form__comment"></textarea>
                             </form>
                                 <button 
                                 class="order__btn"
-                                :class="{disabledBtn: isBtnDisabled}"
                                 form="order-form"
                                 type="submit"
                                 >
@@ -66,7 +65,10 @@
 import headerComponent from "@/components/headerComponent.vue";
 import icon from "@/components/icon.vue";
 import orderItem from '@/components/orderItem.vue';
+import { validationMixin } from 'vuelidate';
+import { required, minLength, email } from 'vuelidate/lib/validators'
 export default{
+    mixins: [validationMixin],
     components:{
         headerComponent,
         icon,
@@ -74,11 +76,6 @@ export default{
     },
     data(){
         return{
-            phoneNumberError: `#F3F3F3`,
-            emailError: `#F3F3F3`,
-            fullNameError: `#F3F3F3`,
-            adresError: `#F3F3F3`,
-            isBtnDisabled: false,
             cart: [],
             order: {
                 phoneNumber: '',
@@ -86,50 +83,38 @@ export default{
                 fullName: '',
                 adress: '',
                 comment: '',
-            }
+            },
         }
     },
+    validations:{
+                order:{
+                    phoneNumber:{ required, minLength: minLength(11)},
+                    email:{required, email},
+                    fullName: {required, minLength: minLength(8)},
+                    adress:{required, minLength: minLength(10)}
+                }
+    },
     methods:{
-        isValidInput(){ // все это костыли
-            if (!(this.order.phoneNumber.length == 11)){
-                this.phoneNumberError = `red`;
-            } else {
-                this.phoneNumberError = `#F3F3F3`;
-            }
-            if (this.order.email.length == 0){
-                this.emailError = `red`;
-            } else {
-                this.emailError = `#F3F3F3`;
-            }
-            if (this.order.fullName.length == 0){
-                this.fullNameError = `red`;
-            } else {
-                this.fullNameError = `#F3F3F3`;
-            }
-            if(this.order.adress.length == 0){
-                this.adresError = `red`;
-            }else {
-                this.adresError = `#F3F3F3`;
+        checkForm(){
+            this.$v.order.$touch()
+            if (!this.$v.order.$error){
+                return true
             }
         },
         createOrder(event){
             event.preventDefault();
-            this.isValidInput() // костыли
-            if(this.phoneNumberError == `#F3F3F3`
-            && this.emailError == `#F3F3F3`
-            && this.fullNameError ==`#F3F3F3`
-            && this.adresError == `#F3F3F3`){
-                this.isBtnDisabled = true // костыли
-                document.querySelector(`.order__btn`).setAttribute(`disabled`, true) //костыли
+            if (this.checkForm()){
                 this.$api.postOrder.postOrder({ 
-                    order: this.order,
-                    cart: this.cart
-                }).then(({data}) => {
-                        })
-                        .catch(e => {
-                            console.log(e)
-                })
+                order: this.order,
+                cart: this.cart
+            }).then(({data}) => {
+                    })
+                    .catch(e => {
+                        console.log(e)
+            })
             }
+            // this.isBtnDisabled = true // костыли
+            // document.querySelector(`.order__btn`).setAttribute(`disabled`, true) //костыли
         }
     },
     mounted(){
@@ -153,25 +138,25 @@ export default{
     form{
         display: flex;
         flex-direction: column;
-        input{
-            margin-bottom: 20px;
-            padding: 15px 10px;
-            border: 1px solid #F3F3F3;
-            border-radius: 100px;
-            outline: none;
-            transition: .3s;
-            width: 100%;
-        }
-        input:hover{
-            border-color: #d3d3d3;
-        }
-        input:focus{
-            border-color: #a7a7a7;
-        }
         input::-webkit-inner-spin-button {
             -webkit-appearance: none;
         }
     }
+}
+.form__number, .form__email, .form__fullName, .form__adress{
+    margin-bottom: 20px;
+    padding: 15px 10px;
+    border: 1px solid #F3F3F3;
+    border-radius: 100px;
+    outline: none;
+    transition: .3s;
+    width: 100%;
+        &:hover{
+            border-color: #d3d3d3;
+        }
+        &:focus{
+            border-color: #a7a7a7;
+        }
 }
 .form__title{
     font-size: 18px;
@@ -190,12 +175,12 @@ export default{
     outline: none;
     transition: .3s;
     width: 100%;
-}
-.form__comment:hover{
-    border-color: #d3d3d3;
-}
-.form__comment:focus{
-    border-color: #a7a7a7;
+    &:hover{
+        border-color: #d3d3d3;
+    }
+    &:focus{
+        border-color: #a7a7a7;
+    }
 }
 .order__btn{
     color: #FFF;
@@ -210,12 +195,21 @@ export default{
     background: #9DD458;
     transition: .3s;
     width: 100%;
+    &:hover{
+        box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
+    }
 }
-.order__btn:hover{
-    box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
-}
-.inputError{
-    border-color: red;
+.isInvalid{
+    border-color: rgb(255, 74, 74);
+    &::placeholder{
+        color: rgb(255, 74, 74);
+    }
+    &:hover{
+        border-color: rgb(255, 40, 40);
+    }
+    &:focus{
+        border-color:rgb(255, 40, 40);
+    }
 }
 .disabledBtn{
     background: #7e7e7e;
